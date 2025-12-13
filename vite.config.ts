d7909e3425079004cd7bfcc0d2bac9f293d97608
xalixilax <xalixilax@gmail.com>
2025-12-12 22:11:54 -0500

    feat: add Tailwind CSS integration with custom button component and utility functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from "@vitejs/plugin-react";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
+	resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 	build: {
 		outDir: "dist",
 		emptyOutDir: true,
